0ad7024a82f4ebf6f60d0e1794844a98998417b1
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2026-08-15 12:20:58 +0700

    Update ins-xray.sh

--- data/ins-xray.sh
+++ data/ins-xray.sh
@@ -64,7 +64,8 @@ touch /var/log/xray/error2.log
 # / / Ambil Xray Core Version Terbaru
 
 # Ambil Xray Core Version Terbaru
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+#latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+latest_version="24.11.5"
 # Installation Xray Core
 # $latest_version
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.7.5/xray-linux-64.zip"
